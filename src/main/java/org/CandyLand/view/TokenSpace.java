c3da070e805d25eab67fcd97ba4f3d23a939442f
Jake Halloran <jakehalloran2@gmail.com>
2017-11-02 22:05:01 -0400

    Added pictures to token spaces instead... it works kinda

--- src/main/java/org/CandyLand/view/TokenSpace.java
+++ src/main/java/org/CandyLand/view/TokenSpace.java
@@ -5,6 +5,7 @@ import javax.swing.border.Border;
 import java.awt.*;
 
 public class TokenSpace extends JButton {
+    private Image image = null;
 
     public TokenSpace(Color color) {
         super();
@@ -13,10 +14,17 @@ public class TokenSpace extends JButton {
         this.setBorder(emptyBorder);
         this.setBackground(color);
     }
+    public TokenSpace(Color color, Image image){
+        this(color);
+        this.setIcon(new ImageIcon(image));
+        this.setDisabledIcon(new ImageIcon(image));
+        this.image = image;
+    }
 
     public void setToken(Token token) {
-       // this.setEnabled(true);
+        this.setEnabled(false);
         this.setIcon(token);
+        this.setDisabledIcon(token);
     }
 
     public Token getToken() {
@@ -30,8 +38,13 @@ public class TokenSpace extends JButton {
     }
 
     public void removeToken() {
+        this.setDisabledIcon(null);
         this.setIcon(null);
         this.setEnabled(false);
+        if(image!=null){
+            this.setDisabledIcon(new ImageIcon(image));
+            this.setIcon(new ImageIcon(image));
+        }
     }
 
 }
