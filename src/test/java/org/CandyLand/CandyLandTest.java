05786345d9274284e473e3cc0ef905783dc1ca90
Ben Anderson <ben.anderson19@gmail.com>
2017-11-18 17:52:16 -0500

    add tests for #72 and #73. close #72 close #73

--- src/test/java/org/CandyLand/CandyLandTest.java
+++ src/test/java/org/CandyLand/CandyLandTest.java
@@ -1,62 +1,8 @@
 package org.CandyLand;
 
-
-
-import org.CandyLand.view.*;
-import org.CandyLand.model.*;
 import org.junit.Test;
-import javax.swing.*;
-import java.awt.*;
-import java.io.IOException;
-
 import static org.junit.Assert.*;
 
 public class CandyLandTest {
-
-
-
-    /*
-    *US: Game Window Test
-    *
-    * windowExists() - confirm generation of game window
-    * cardExists() - confirm visibility of card pile
-    */
-//    @Test
-//    public void windowExists(){
-//
-//        try {
-//            MainFrame mainFrame = new MainFrame();
-//            assertEquals(mainFrame.getFrame().isVisible(), true);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//
-//    @Test
-//    public void cardExists(){
-//
-//
-//
-//        try {
-////            Robot r = new Robot();
-////            r.keyPress(java.awt.event.KeyEvent.VK_ENTER);
-//            MainFrame mainFrame = new MainFrame();
-////            r.keyRelease(java.awt.event.KeyEvent.VK_ENTER);
-//            assertEquals(mainFrame.getPanel().isVisible(), true);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-////        } catch (AWTException e) {
-////            e.printStackTrace();
-//       }
-//
-//    }
-
-
-
-
-
-
-
 
 }
